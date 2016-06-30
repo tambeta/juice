5ecd3b584ec7a240a7a5ac08d08fa02806bf95b7
Tambet Arak <tambet.arak+nospam@gmail.com>
2016-07-01 00:00:00 +0300

    save / load state; main script refactoring

--- juice.py
+++ juice.py
@@ -2,6 +2,7 @@
 
 import argparse
 import array
+import pickle
 import pprint
 import random
 import sys
@@ -13,6 +14,10 @@ from juice.heightmap import Heightmap
 from juice.terrain import Terrain
 from juice.terrainlayer import \
     TerrainLayer, RiverLayer, SeaLayer, BiomeLayer, CityLayer
+
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+TERRAIN_DIM = 2**7
 
 _g = {}
 
@@ -42,21 +47,23 @@ def parse_command_line():
 		"-t", "--timing", action="store_true",
         help="Timing / profiling mode, exit after terrain generation"
     )
+	parser.add_argument(
+		"-s", "--save", type=str, help="Save a map to file")
+	parser.add_argument(
+		"-l", "--load", type=str, help="Load a saved map")
 	return parser.parse_args()
 
-def main():
-    GAME_WIDTH = 800
-    GAME_HEIGHT = 600
-    TERRAIN_DIM = 2**7
+def save_state(obj, fn):
+    f = open(fn, "wb")
+    pickle.dump(obj, f)
     
-    args = parse_command_line()
-    randseed = args.random_seed \
-        if args.random_seed \
-        else random.randint(1, 10000)
-    scaling = min(GAME_WIDTH, GAME_HEIGHT) // TERRAIN_DIM
-    window = pyglet.window.Window(GAME_WIDTH, GAME_HEIGHT)
+def load_state(fn):
+    f = open(fn, "rb")
+    return pickle.load(f)
+
+def generate(randseed=None):
     
-    print("random seed:", randseed)
+    """ Generate a Terrain and return it. """
     
     terr = Terrain(TERRAIN_DIM, randseed=randseed)
     terr.add_layer(SeaLayer(randseed=randseed))
@@ -64,6 +71,28 @@ def main():
     terr.add_layer(BiomeLayer(randseed=randseed))
     terr.add_layer(CityLayer(randseed=randseed))
     terr.generate(post_generate_cb=timed_print)
+    
+    return terr 
+
+def main():
+    args = parse_command_line()
+    randseed = args.random_seed \
+        if args.random_seed \
+        else random.randint(1, 10000)
+    scaling = min(GAME_WIDTH, GAME_HEIGHT) // TERRAIN_DIM
+    window = pyglet.window.Window(GAME_WIDTH, GAME_HEIGHT)
+    terr = None
+    
+    print("random seed:", randseed)
+    print("scaling:", scaling)
+    
+    if (not args.load):
+        terr = generate(randseed)
+        if (args.save):
+            save_state(terr, args.save)
+    else:
+        print("Loading map from `{}`".format(args.load))
+        terr = load_state(args.load)
     
     if (args.timing):
         sys.exit(0)
